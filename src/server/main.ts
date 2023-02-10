d7386758eb5cf3cbf2db0958edce5944753384ff
Mathieu Florentin <mathieu.florentin27@gmail.com>
2023-02-10 15:24:16 +0100

    First implementation of GET request

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import headToHead from "./data/fixtures/headtohead.json";
 
 const app = express();
-
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
-});
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
 );
+
+app.get("/players", (_, res) => {
+  res.send(res.json({ data: headToHead }));
+});
